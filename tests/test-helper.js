755b1036b778d10312b29c19482aa91b6bc50840
BarbieSisa <131451616+BarbieSisa@users.noreply.github.com>
2024-05-22 11:50:32 +0300

    Add files via upload

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'irrigation-system/app';
+import config from 'irrigation-system/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
